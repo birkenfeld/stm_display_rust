616bb6104675928dfd3c27ce64fde59f146c5228
Georg Brandl <g.brandl@fz-juelich.de>
2025-09-09 16:50:02 +0200

    fix utils

--- util/clock.py
+++ util/clock.py
@@ -8,17 +8,21 @@ from drawlib import Display
 
 d = Display(serial.Serial(sys.argv[1], baudrate=115200))
 
-color = [0, 8, 7, 15]
+palette = [0, 8, 7, 15]
 
 d.reset_clip()
 d.clear(0)
 d.set_font(3)
 d.set_color([0, 235, 240, 245])
-d.pos_text((130, 4), ":")
-d.pos_text((290, 4), ":")
-d.set_attrs(0, (20, 4), color, 3)
-d.set_attrs(1, (180, 4), color, 3)
-d.set_attrs(2, (340, 4), color, 3)
+# no colon in vlarge font... fake it with two dots
+d.pos_text((130, 20), ".")
+d.copy_rect((130, 60), (167, 83), (130, 40))
+d.pos_text((290, 20), ".")
+d.copy_rect((290, 60), (327, 83), (290, 40))
+
+d.set_attrs(0, (30, 30), palette, 3)
+d.set_attrs(1, (190, 30), palette, 3)
+d.set_attrs(2, (350, 30), palette, 3)
 d.switch_graphics()
 
 while True:
